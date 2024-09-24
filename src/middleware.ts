eac1a711f614ece6cb8972c5ba6f5ab28f04c7bf
Gustavo Jose <gjose2980@gmail.com>
2024-09-24 02:25:59 -0300

    Pequenos ajustes na autenticacao

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,23 @@ import type { NextRequest } from "next/server";
 var jwt = require('jsonwebtoken');
 
 const SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET || "secret-key";
-
+console.log("SECRET_KEY:", SECRET_KEY);
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("jwt-token")?.toString();
+  console.log("Middleware de autenticação");
+  const token = req.cookies.get("token")?.toString();
+
+  console.log("Token:", token);
 
   if (!token) {
+    console.log("Token não encontrado");
     return NextResponse.redirect(
       new URL(`/auth/login`, req.url),
     );
   }
 
   try {
+    console.log("Verificando o token:", token);
+    // VERIFICA SE O TOKEN É VALIDO
     jwt.verify(token, SECRET_KEY);
 
     // SE FOR VALIDO PASSA
